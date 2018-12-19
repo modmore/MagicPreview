c4c646bd63d39e076a2882ddb6a77bcc12ec5d30
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2018-12-19 18:37:27 +0100

    Add German translation

---
+++ core/components/magicpreview/lexicon/de/default.inc.php
@@ -0,0 +1,8 @@
+<?php
+$_lang['magicpreview'] = 'Magische Vorschau';
+$_lang['magicpreview.preview'] = 'Vorschau für ';
+$_lang['magicpreview.preparing_preview'] = 'Ihre Vorschau wird vorbereitet...';
+$_lang['magicpreview.bp_full'] = 'Volle Breite';
+$_lang['magicpreview.bp_desktop'] = 'Desktop';
+$_lang['magicpreview.bp_tablet'] = 'Tablet';
+$_lang['magicpreview.bp_mobile'] = 'Mobil';
