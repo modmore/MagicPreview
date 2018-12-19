23f884c8b5477b299868272c4e4dfef948284197
Mathias Dannevang <mathias@dannevang.org>
2018-12-19 15:07:13 +0100

    Add Danish translation

---
+++ core/components/magicpreview/lexicon/da/default.inc.php
@@ -0,0 +1,9 @@
+<?php
+$_lang['magicpreview'] = 'Magisk forhåndsvisning';
+$_lang['magicpreview.preview'] = 'Forhåndsvisning af ';
+$_lang['magicpreview.preparing_preview'] = 'Forbereder din forhåndsvisning...';
+
+$_lang['magicpreview.bp_full'] = 'Fuld størrelse';
+$_lang['magicpreview.bp_desktop'] = 'Desktop';
+$_lang['magicpreview.bp_tablet'] = 'Tablet';
+$_lang['magicpreview.bp_mobile'] = 'Mobil';
